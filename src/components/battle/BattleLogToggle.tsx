0874ac2450f45fb7015b133ffb0df84a53e64e06
Ethan <Ethan.Gold-Steinberg@protonmail.com>
2025-08-29 09:19:16 -0400

    altered deploy

--- src/components/battle/BattleLogToggle.tsx
+++ src/components/battle/BattleLogToggle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BattleTurn } from '../utils/interefaces';
+import { ActiveBattle as NewActiveBattle, BattleTurn } from '../../utils/interefaces';
 
 interface BattleLogToggleProps {
   turns: BattleTurn[];
